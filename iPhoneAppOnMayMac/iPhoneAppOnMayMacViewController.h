e35e8094b79109959243fb83c8e7d0b9670bad4b
jagdish-globalsoft <jgadishmehra@Jgadishs-MacBook-Air.local>
2013-09-15 17:24:22 +1000

    Revert "Second from within Xcode"
    This reverts commit 3b5d09614e19f17db455cb87be142b13f8edf5de.

--- iPhoneAppOnMayMac/iPhoneAppOnMayMacViewController.h
+++ iPhoneAppOnMayMac/iPhoneAppOnMayMacViewController.h
@@ -4,8 +4,7 @@
 //
 //  Created by Jagdish Mehra on 15/09/13.
 //  Copyright (c) 2013 GlobalSoft Technologies. All rights reserved.
-// Jagdish added this comment
-//Now I am adding this line
+//
 
 #import <UIKit/UIKit.h>
 
