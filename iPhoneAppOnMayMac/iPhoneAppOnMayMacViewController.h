1b489d8cc9ec99d9cccf0407bcbfbc2bb5835685
jagdish-globalsoft <jgadishmehra@Jgadishs-MacBook-Air.local>
2013-09-15 18:07:59 +1000

    Another checkin

--- iPhoneAppOnMayMac/iPhoneAppOnMayMacViewController.h
+++ iPhoneAppOnMayMac/iPhoneAppOnMayMacViewController.h
@@ -4,9 +4,8 @@
 //
 //  Created by Jagdish Mehra on 15/09/13.
 //  Copyright (c) 2013 GlobalSoft Technologies. All rights reserved.
-// This is new
-
-
+// Comments added by Jagdish
+//Another comment added
 
 #import <UIKit/UIKit.h>
 
